37c166ec23d0d3514e12e78b830afc87b8043439
VasuShandar <49501072+VasuShandar@users.noreply.github.com>
2020-09-16 09:55:17 -0700

    Add files via upload

---
+++ practice.m
@@ -0,0 +1,12 @@
+%%Built graph using cereal data graphing fat vs protein and sorting by
+%%brand 
+load cereal.mat;
+g=gramm('x', Fat, 'y', Protein, 'color', Name);
+g.geom_point();
+g.stat_glm();
+g.set_names('x', 'Fat', 'y', 'Protein', 'color', 'Brand');
+g.set_title('Cereal Healthiness');
+figure('Position', [100, 100, 800, 400]);
+g.draw();
+%% Graph using car mileage data 
+load flu.mat;
